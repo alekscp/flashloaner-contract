2cafb09325ca7bb7e521690128534a211efa449b
Aleks Clapin-Pepin <aclapinpepin@gmail.com>
2021-03-28 18:53:36 +0545

    Adjust migration and load dotenv

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -7,7 +7,7 @@ module.exports = function (deployer) {
   deployer.link(SafeMath, UniswapV2Library);
   deployer.deploy(UniswapV2Library);
   deployer.link(UniswapV2Library, FlashLoaner);
-  deployer.deploy(FlashLoaner, '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f', '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D', '0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F');  
+  deployer.deploy(FlashLoaner, process.env.PANCAKE_FACTORY, process.env.PANCAKE_ROUTER, process.env.JUL_ROUTER);  
 };
 
 
